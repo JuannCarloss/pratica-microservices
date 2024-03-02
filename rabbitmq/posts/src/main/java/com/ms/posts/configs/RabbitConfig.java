ffaeacf77bd7dfc1ab8a7c7f9bb7672dd440462a
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-02 12:50:45 -0300

    feat: rabbitmq configuration

---
+++ rabbitmq/posts/src/main/java/com/ms/posts/configs/RabbitConfig.java
@@ -0,0 +1,26 @@
+package com.ms.posts.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Value("${broker.queue.posts.name}")
+    private String queue;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queue, true);
+    }
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+}
