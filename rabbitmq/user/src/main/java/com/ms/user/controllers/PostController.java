d14a7614b89b1350e469d59e562100ff6233ea36
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-02 12:40:38 -0300

    feat: post method to send the payload

---
+++ rabbitmq/user/src/main/java/com/ms/user/controllers/PostController.java
@@ -0,0 +1,23 @@
+package com.ms.user.controllers;
+
+import com.ms.user.dtos.PostDTO;
+import com.ms.user.entities.User;
+import com.ms.user.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posting")
+public class PostController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("{username}")
+    public void newPost(@RequestBody PostDTO data, @PathVariable("username") String username) throws Exception {
+        ResponseEntity.status(HttpStatus.OK).body(userService.publishPost(data, username));
+    }
+}
