57b56beac5d6a425a455d41f3c127f726c9ff302
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-02 12:50:24 -0300

    feat: initial config

---
+++ rabbitmq/posts/src/main/java/com/ms/posts/PostsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsApplication.class, args);
+	}
+
+}
