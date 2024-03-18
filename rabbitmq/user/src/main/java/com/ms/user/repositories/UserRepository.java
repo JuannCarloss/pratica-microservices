8db35be979cba38f822b0e558ae32c0273295713
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-17 22:03:03 -0300

    feat: changing findByUsername method to return UserDetails

--- rabbitmq/user/src/main/java/com/ms/user/repositories/UserRepository.java
+++ rabbitmq/user/src/main/java/com/ms/user/repositories/UserRepository.java
@@ -2,9 +2,10 @@ package com.ms.user.repositories;
 
 import com.ms.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUsername(String username);
+    UserDetails findByUsername(String username);
 }
