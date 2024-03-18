c1869565c47eaf208060e5e1290ae04ef430e1bf
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-17 22:04:06 -0300

    feat: implementing pagination

--- rabbitmq/user/src/main/java/com/ms/user/services/UserService.java
+++ rabbitmq/user/src/main/java/com/ms/user/services/UserService.java
@@ -1,12 +1,15 @@
 package com.ms.user.services;
 
 import com.ms.user.dtos.PostDTO;
+import com.ms.user.dtos.RegisterDTO;
 import com.ms.user.dtos.UserDTO;
 import com.ms.user.entities.User;
 import com.ms.user.producers.PostProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,12 +22,15 @@ public class UserService {
     private PostProducer postProducer;
 
 
-    public User postUser(UserDTO userDTO){
-        if (userRepository.findByUsername(userDTO.username()) != null){
+    public User postUser(User user){
+        if (userRepository.findByUsername(user.getUsername()) != null){
             return null;
         }
-        var user = new User(userDTO);
         return userRepository.save(user);
+    }
+
+    public Page<User> getAll(Pageable pageable){
+        return userRepository.findAll(pageable);
     }
 
 
