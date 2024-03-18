a759bfffb7efeb6c46f6a7432d234d9d9d7e70c2
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-17 21:58:44 -0300

    feat: AuthenticationService loadByUsername

---
+++ rabbitmq/user/src/main/java/com/ms/user/services/AuthenticationService.java
@@ -0,0 +1,22 @@
+package com.ms.user.services;
+
+import com.ms.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepository repository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
