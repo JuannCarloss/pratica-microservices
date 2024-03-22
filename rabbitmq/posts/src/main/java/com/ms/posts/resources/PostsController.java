ad4c644e890cae0fd0188d8f25ecf946b68367a1
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-21 21:36:46 -0300

    feat: posts controller with pageable list method

---
+++ rabbitmq/posts/src/main/java/com/ms/posts/resources/PostsController.java
@@ -0,0 +1,40 @@
+package com.ms.posts.resources;
+
+import com.ms.posts.documents.PostDocument;
+import com.ms.posts.dtos.PostDTO;
+import com.ms.posts.services.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostsController {
+
+    @Autowired
+    private PostsService service;
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody PostDTO data){
+        var save = service.newPost(data);
+        return ResponseEntity.ok().body(save);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable("id") String id, @RequestBody PostDocument post){
+        var save = service.updatePost(id, post);
+        return ResponseEntity.ok().body(save);
+    }
+
+    @GetMapping
+    public ResponseEntity findAll(@RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "3") int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PostDocument> list = service.findAll(pageable);
+        return ResponseEntity.ok().body(list);
+
+    }
+}
