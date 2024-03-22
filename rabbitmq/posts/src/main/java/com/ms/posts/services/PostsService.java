16d629899a90ac03661562a411dd0bf182df4104
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-21 21:37:12 -0300

    feat: put and get methods

--- rabbitmq/posts/src/main/java/com/ms/posts/services/PostsService.java
+++ rabbitmq/posts/src/main/java/com/ms/posts/services/PostsService.java
@@ -4,7 +4,11 @@ import com.ms.posts.documents.PostDocument;
 import com.ms.posts.dtos.PostDTO;
 import com.ms.posts.repositories.PostsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PostsService {
@@ -15,5 +19,20 @@ public class PostsService {
     public PostDocument newPost(PostDTO data){
         var save = new PostDocument(data);
         return postsRepository.save(save);
+    }
+
+    public PostDocument updatePost(String id, PostDocument updated){
+        Optional<PostDocument> optional = postsRepository.findById(id);
+        if (optional.isPresent()){
+            var post = optional.get();
+            post.setTitle(updated.getTitle());
+            post.setBody(updated.getBody());
+            return postsRepository.save(post);
+        }
+        return null;
+    }
+
+    public Page<PostDocument> findAll(Pageable pageable){
+        return postsRepository.findAll(pageable);
     }
 }
