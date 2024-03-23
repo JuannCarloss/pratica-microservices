4e186e09d6594d00f67f6beae208123855edc32d
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-23 16:38:12 -0300

    feat: extending CustomQuerydslPredicateExecutor

--- rabbitmq/user/src/main/java/com/ms/user/repositories/UserRepository.java
+++ rabbitmq/user/src/main/java/com/ms/user/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ms.user.repositories;
 
+import com.ms.user.enterprise.CustomQuerydslPredicateExecutor;
 import com.ms.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, CustomQuerydslPredicateExecutor<User> {
     UserDetails findByUsername(String username);
 }
