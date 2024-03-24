bd457615ec0008d37e0c702497788dcd6f9c8d37
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-24 14:16:21 -0300

    feat: implementing emailProducer to send emails

--- rabbitmq/user/src/main/java/com/ms/user/services/UserService.java
+++ rabbitmq/user/src/main/java/com/ms/user/services/UserService.java
@@ -1,17 +1,16 @@
 package com.ms.user.services;
 
 import com.ms.user.dtos.PostDTO;
-import com.ms.user.dtos.RegisterDTO;
-import com.ms.user.dtos.UserDTO;
 import com.ms.user.entities.User;
+import com.ms.user.producers.EmailProducer;
 import com.ms.user.producers.PostProducer;
 import com.ms.user.repositories.UserRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -22,14 +21,22 @@ public class UserService {
     @Autowired
     private PostProducer postProducer;
 
+    @Autowired
+    private EmailProducer emailProducer;
 
+
+    @Transactional
     public User postUser(User user){
         if (userRepository.findByUsername(user.getUsername()) != null){
             return null;
         }
-        return userRepository.save(user);
+        var save = userRepository.save(user);
+        emailProducer.sendEmail(user);
+        return save;
     }
 
+
+    @Cacheable(value = "users")
     public Page<User> getAll(String filter, Pageable pageable){
         return userRepository.findAll(filter, User.class, pageable);
     }
