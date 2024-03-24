71ce20b35a088ee23c29c79b7f26be4779fe27ba
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-24 14:18:13 -0300

    feat: implementing email entity

---
+++ rabbitmq/email/src/main/java/com/ms/email/entities/Email.java
@@ -0,0 +1,39 @@
+package com.ms.email.entities;
+
+import com.ms.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "emails")
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "email_from")
+    private String emailFrom;
+    @Column(name = "email_to")
+    private String emailTo;
+    @Column(name = "subject")
+    private String subject;
+    @Column(name = "body", columnDefinition = "TEXT")
+    private String body;
+    @Column(name = "send_date")
+    private LocalDateTime emailSendDate;
+    @Column(name = "email_status")
+    private StatusEmail statusEmail;
+
+
+}
