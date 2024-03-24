395fcae79e393fa43735944d41626e2baddcf1a9
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-24 16:13:13 -0300

    feat: implementing method findByEmail

--- rabbitmq/user/src/main/java/com/ms/user/repositories/UserRepository.java
+++ rabbitmq/user/src/main/java/com/ms/user/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, CustomQuerydslPredicateExecutor<User> {
     UserDetails findByUsername(String username);
+    UserDetails findByEmail(String email);
 }
