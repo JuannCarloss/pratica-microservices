27809d1a73a3ef96180d7d44e83ed4d3b46a653e
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2024-03-26 23:44:51 -0300

    feat: redis simple cache configuration

---
+++ rabbitmq/posts/src/main/java/com/ms/posts/configs/RedisCacheConfig.java
@@ -0,0 +1,26 @@
+package com.ms.posts.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
+
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration defaultCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+
+}
